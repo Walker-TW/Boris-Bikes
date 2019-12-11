bd6cf10fb4a11951e1f31eaaaa6502bec488ab49
Walker-TW <thomas.w.walker@live.co.uk>
2019-12-11 17:41:18 +0000

    Default capacity added as well as variability in capacity

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,15 +1,26 @@
 class DockingStation
   attr_reader :bike_rack
-  def initialize
+
+  def initialize(capacity = 20)
     @bike_rack = []
-    @capacity = 20
+    @capacity = capacity
   end
   def release_bike
-    raise 'No bike is present' if @bike_rack.empty?
+    raise 'No bike is present' if empty?
     @bike_rack.pop
   end
+
   def dock(bike)
-    raise 'No docking station are free' unless @bike_rack.size < @capacity
+    raise 'No docking station are free' if full?
     @bike_rack << bike
+  end
+
+  private
+  def full?
+    @bike_rack.size == @capacity
+  end
+
+  def empty?
+    @bike_rack.empty?
   end
 end
