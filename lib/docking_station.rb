50e75e8f6b55e7c56458a378e7cf86fdfd993b20
Walker-TW <thomas.w.walker@live.co.uk>
2019-12-11 16:06:16 +0000

    Added capacity tests

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,14 +4,15 @@ class DockingStation
   attr_reader :bike
 
   def dock(bike)
+    raise "Dock is full" unless @bike == nil
     @bike = bike
   end
 
   def release_bike
-    return Bike.new
+    @bike
   end
 
   def bike_exist?
-    fail "No bike"
+    fail "No bike" unless @bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,11 +6,11 @@ describe DockingStation do
     it {is_expected.to respond_to('release_bike')} 
   
     it "release a working bike" do
-      bike = station.release_bike
+      bike = Bike.new
       expect(bike).to be_working
     end
 
-    it "socks a bike" do
+    it "stocks a bike" do
     expect(station.dock(Bike.new)).to be_a(Bike)
     end
 
@@ -22,7 +22,20 @@ describe DockingStation do
       expect {station.bike_exist?}.to raise_error("No bike")
     end
 
-end
+    describe "release_bike" do
+      it "doesnt work" do
+        station.dock(Bike.new)
+        expect(station.release_bike).to be_a(Bike)
+        end
+      end
 
+  describe "#dock" do
+
+  it "Capacity is full" do
+    station.dock(Bike.new)
+    expect{station.dock(Bike.new)}.to raise_error("Dock is full")
+  end
+end
+end
 
 
